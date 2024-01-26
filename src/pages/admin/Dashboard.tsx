1aebb21db40525ae0258c617c8f9fd16625bcef0
tovare680 <tovare680@intt.gob.ve>
2024-01-26 16:52:42 -0400

    Add SideBar Menus

--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -1,3 +1,5 @@
+import { Sidebar } from "@/components/SideBar/SideBar";
+import SidebarDemo from "@/components/SideBar/SideBarDemo";
 import { ModeToggle } from "@/components/Theme/ToggleButton";
 import AdminLayout from "@/layouts/AdminLayout";
 // import { Link } from "react-router-dom";
@@ -6,6 +8,7 @@ import AdminLayout from "@/layouts/AdminLayout";
 export function Dashboard() {
   return (
     <AdminLayout>
+        <SidebarDemo/>
         <ModeToggle/>
         <p>Dashboard</p>
     </AdminLayout>
